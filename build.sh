c0e819455e9bfbfef7dc748b5cab420e6afdb148
Raju <rudram.raju@tern-group.com>
2026-01-01 02:25:57 +0530

    build-fixes

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+echo "Installing root dependencies..."
+npm install
+
+echo "Installing backend dependencies..."
+cd backend && npm install && cd ..
+
+echo "Build complete!"
+
